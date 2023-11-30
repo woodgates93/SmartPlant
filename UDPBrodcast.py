7ebb27d86b7f6658716685924b4d8c5f53e339b6
Peter Nordentoft <11867848+fusrap@users.noreply.github.com>
2023-11-30 12:59:55 +0100

    Update UDPBrodcast.py
    Udvidet med senshat temp måling

--- UDPBrodcast.py
+++ UDPBrodcast.py
@@ -1,15 +1,27 @@
 from socket import *
-#from _sensehat import SenseHat
+from sense_hat import SenseHat
+from time import sleep
 
 servername = 'localhost'
 serverport = 6666
+
+
+#methods
+def getMeasurement():
+    sensehat = SenseHat()
+    measurement = sensehat.get_temperature_from_humidity()
+    return measurement
+
+
 
 broadcastsocket = socket (AF_INET, SOCK_DGRAM)
 
 broadcastsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-tekst = 'hej'.encode('utf-8')
-broadcastsocket.sendto(tekst, ('<broadcast>', serverport))
-
+while True:
+    data = str(getMeasurement())
+    encodeddata = data.encode('utf-8')
+    broadcastsocket.sendto(encodeddata, ('<broadcast>', serverport))
+    sleep(1)
 
 
