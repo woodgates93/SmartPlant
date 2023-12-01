b0d44f8d3c29af6ef7855641b7a7cad26cd320af
woodgates93 <113350688+woodgates93@users.noreply.github.com>
2023-12-01 11:15:01 +0100

    Add files via upload
    API + Lib

---
+++ SmartPlantApi/SmartPlantApi/Controllers/TempController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartPlantLib.Collection;
+using SmartPlantLib.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartPlantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TempController : ControllerBase
+    {
+        private TemperatureRepo _data;
+
+        public TempController(TemperatureRepo repo)
+        {
+            _data = repo;
+        }
+
+
+        // GET: api/<TempController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Get()
+        {
+            IEnumerable<Temperature> temperatures = _data.GetAll();
+
+            if (temperatures.ToList().Count== 0)
+            {
+                return NoContent();
+            }
+            return Ok(temperatures);
+        }
+
+        // GET api/<TempController>/5
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Temperature temperature = _data.GetById(id);
+                return Ok(temperature);
+            }
+            catch (KeyNotFoundException knfe)
+            {
+                return NotFound();
+            }
+            
+        }
+
+        // POST api/<TempController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Post([FromBody] Temperature value)
+        {
+            Temperature temperature = _data.Add(value);
+            return Created($"api/Temp/{temperature.Id}", temperature);
+        }
+
+        // PUT api/<TempController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TempController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SmartPlantLib/SmartPlantLib/Model/Temperature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPlantLib.Model
+{
+    public class Temperature
+    {
+        private int _id;
+        private double _temperature;
+
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public double Temp { get { return _temperature; } set { _temperature = value; } }
+
+
+        public Temperature() {
+            _id= 0;
+            _temperature = 0;
+        }
+
+        public Temperature(int id, double temperature)
+        {
+            _id = id;
+            _temperature = temperature;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()},{nameof(Temp)}={Temp.ToString()}}}";
+        }
+
+
+
+
+    }
+}
