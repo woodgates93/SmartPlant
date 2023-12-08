86b90fbf5c3b424b2077a96fb5c0b70115b29163
pete74s9 <pete74s9@users.noreply.github.com>
2023-12-08 09:29:26 +0100

    Updated proxy: samlet data

--- Proxy.py
+++ Proxy.py
@@ -1,25 +1,42 @@
 import requests 
 from socket import *
 import threading
+import json
 
-
-RECEVING_PORT = 6666
+RECEIVING_PORT = 45600
 BUFFER_SIZE = 1024
 REST_URL = 'http://localhost:5053/api/Temp'
 
+data_1 = {}
+data_2 = {}
+
 server = socket(AF_INET, SOCK_DGRAM)
-server.bind(('', RECEVING_PORT))
+server.bind(('', RECEIVING_PORT))
 
 def do_client():
-        data, client_addr = server.recvfrom(BUFFER_SIZE)
-        t = data.decode()
-        print(t)
+    global data_1, data_2  # Declare data_1 and data_2 as global variables
 
+    data, addr = server.recvfrom(BUFFER_SIZE)
+
+    temp_data = json.loads(data.decode())
+    if temp_data['id'] % 2 != 0:
+        data_1 = temp_data
+    else:
+        data_2 = temp_data 
+
+    if not data_1 and not data_2:
+        data_1.update(data_2)
+        data_1.clear()
+        data_2.clear()
+    
+    print(data_1)
+
+    try:
         headers = {'Content-type': 'application/json'}
-        r = requests.post(REST_URL, data=t, headers=headers)
-        
-        print(r)
+        r = requests.post(REST_URL, data=json.dumps(data_1), headers=headers)
+    except Exception as e:
+        print(f"Error: {e}")
 
 while True:
-        thread = threading.Thread(target = do_client())
-        thread.start()
+    thread = threading.Thread(target=do_client)
+    thread.start()
