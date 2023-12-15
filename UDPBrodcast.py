cf404b80a2c6a9b671fc1f179229787a63ea9b0e
FrederikG <112688428+Gru999@users.noreply.github.com>
2023-12-15 13:06:15 +0100

    Opdateret UDPBroadcast til Raspberry pi versionen - UDPBrodcast.py

--- UDPBrodcast.py
+++ UDPBrodcast.py
@@ -1,26 +1,16 @@
 from socket import *
-from sense_hat import SenseHat 
+#from sense_hat import SenseHat 
 from time import sleep
+from SerialConnection import readSerialData
 
 broadcastPort = 6666
 
-
-#methods
-def getMeasurement():
-    sensehat = SenseHat()
-    measurement = sensehat.get_temperature_from_humidity()
-    return measurement
-
-
-
 broadcastsocket = socket (AF_INET, SOCK_DGRAM)
-
 broadcastsocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 while True:
-    data = str(getMeasurement())
+    data = readSerialData()
     encodeddata = data.encode('utf-8')
-    broadcastsocket.sendto(encodeddata, ('<broadcast>', broadcastPort))
+    broadcastsocket.sendto(encodeddata, ('255.255.255.255', broadcastPort))
     sleep(1)
-
 
